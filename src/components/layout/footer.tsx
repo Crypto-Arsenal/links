39da136fb06d920907579e754ad7355cf089701f
Alan Wu <ac2wu@edu.uwaterloo.ca>
2023-04-07 13:28:28 +1000

    feat: fix links

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,7 +1,7 @@
 import { Center, Text } from "@chakra-ui/react";
 
 const Footer = () => {
-  return <Center mt={5} > <Text fontWeight={'thin'} fontSize="xs"> Made with ❤️ by Crypto Arsenal 2022 </Text></Center>;
+  return <Center mt={5} > <Text fontWeight={'thin'} fontSize="xs"> Made with ❤️ by Crypto Arsenal 2023 </Text></Center>;
 };
 
 export default Footer;
